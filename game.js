5596f587afe73fbadefb6fc3cb4bb5d0f3a4f3cb
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-12-25 14:34:18 +0530

    Learning Javascript Day 1

---
+++ game.js
@@ -0,0 +1,16 @@
+// 'use strict'
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent  = "Hello World 😭🎖️💯"
+
+
+// var element = document.querySelector('.guess');
+// element.value = 23;
+
+// if (element.value == 23) {
+//     var scoreElement = document.querySelector('.score');
+//     scoreElement.textContent = 100; // Update the content of the element to show 100
+// }
+
+
