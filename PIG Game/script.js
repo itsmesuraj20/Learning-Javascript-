99d9d05f6deb22346922e349f80faa79aa434445
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-12-28 00:12:50 +0530

    JS Learning

--- PIG Game/script.js
+++ PIG Game/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 
 const score0El = document.querySelector('#score--0');
 const score1El  = document.getElementById('score--1');
@@ -33,15 +36,13 @@ btnRoll.addEventListener('click' , function (){
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
-        current0El.textContent = currentScore;  
-
     }
     else{
         //Switch to next player[]
         document.getElementById(`current--${activePlayer}`).textContent = 0;
-
-         
-        activePlayer = activePlayer === 0 ? 1 : 0;
         currentScore = 0 ;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
