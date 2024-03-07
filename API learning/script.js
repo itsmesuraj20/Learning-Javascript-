75dea54ef016900f3269075fa55123cc4201469c
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-03-07 18:03:36 +0530

    Weather API App done

--- API learning/script.js
+++ API learning/script.js
@@ -1,6 +1,11 @@
 const button = document.getElementById("search-button");
 const input = document.getElementById("city-input");
 
+const cityName = document.getElementById("city-name");
+const cityTime = document.getElementById("city-time");
+const cityTemp = document.getElementById("city-temp");
+
+
 async function getData(cityName){
     const promise =await fetch(
         `http://api.weatherapi.com/v1/current.json?key=0ec5c680d0c04d41b73115348240703&q=${cityName}&aqi=yes`
@@ -11,7 +16,10 @@ async function getData(cityName){
 button.addEventListener('click',async ()=>{
     const value = input.value;
     const result = await getData(value);
-    console.log(result);
+    cityName.innerText =`${result.location.name} , ${result.location.region} - ${result.location.country}`;
+
+    cityTime.innerText = result.location.localtime;
+    cityTemp.innerText = result.current.temp_c;
 });
 
 
